bf62e364520011b084997f1c079cb0e26af6d49a
hackycy <qa894178522@qq.com>
2022-01-13 16:38:48 +0800

    fix: using came case comp

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <hello-world msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
