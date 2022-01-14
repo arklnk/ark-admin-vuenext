14c5441f7adf0b4c2861aeb7eadb8726cb174a3a
hackycy <qa894178522@qq.com>
2022-01-14 15:05:04 +0800

    chore: import windicss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import windicss from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), windicss()],
 })
