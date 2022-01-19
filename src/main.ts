8f2bbe1b24fe79c917e60c47382b16e1ef50945e
hackycy <qa894178522@qq.com>
2022-01-19 11:40:29 +0800

    feat: register pinia

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import 'virtual:windi.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { registerPlugin } from '/@/core/use'
+import { setupStore } from '/@/store'
 
 async function bootstrap() {
   const app = createApp(App)
+
+  // store
+  setupStore(app)
 
   // register plugin
   registerPlugin(app)
