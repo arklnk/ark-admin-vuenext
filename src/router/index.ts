5c9197ac2522f37980f982b58bd1a0d17ed5d74d
hackycy <qa894178522@qq.com>
2022-01-19 14:30:48 +0800

    feat: add createRouter scrollBehavior params

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: [],
-  strict: true
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 })
 })
 
 export function setupRouter(app: App<Element>) {
