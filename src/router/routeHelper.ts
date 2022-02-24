47f067195b953d5598fc91a0b5eefdda32d448b8
hackycy <qa894178522@qq.com>
2022-02-24 12:11:23 +0800

    feat: impl routeHelper

--- src/router/routeHelper.ts
+++ src/router/routeHelper.ts
@@ -3,7 +3,52 @@ import type { RouteRecordRaw } from 'vue-router'
 
 import { ParentLayout, EmptyLayout } from '/@/router/basicRoutes'
 import { isUrl } from '/@/utils/is'
-import { routeModuleMap } from '/@/router/asyncRoutes'
+import { routeModuleMap } from './routeModule'
+
+/**
+ * Filter asynchronous routing tables by recursion
+ */
+export function filterAsyncRoutes(routes: Menu[], parentRoute: Menu): RouteRecordRaw[] {
+  const asyncRoutes: RouteRecordRaw[] = []
+
+  routes.forEach((routeItem) => {
+    if (routeItem.type === 2 || routeItem.isShow) {
+      // 权限 或者 隐藏则直接pass
+      return
+    }
+
+    let realRoute: RouteRecordRaw | null = null
+    if (!parentRoute && !routeItem.parentId && routeItem.type === 1) {
+      // 根菜单
+      realRoute = createRouteItem(routeItem, true)
+    } else if (!parentRoute && !routeItem.parentId && routeItem.type === 0) {
+      // 目录
+      const childRoutes = filterAsyncRoutes(routes, routeItem)
+      realRoute = createRouteItem(routeItem, true)
+      if (childRoutes && childRoutes.length > 0) {
+        realRoute!.redirect = childRoutes[0].path
+        realRoute!.children = childRoutes
+      }
+    } else if (parentRoute && parentRoute.id === routeItem.parentId && routeItem.type === 1) {
+      // 子菜单
+      realRoute = createRouteItem(routeItem, false)
+    } else if (parentRoute && parentRoute.id === routeItem.parentId && routeItem.type === 0) {
+      // 如果还是目录，继续递归
+      const childRoute = filterAsyncRoutes(routes, routeItem)
+      realRoute = createRouteItem(routeItem, false)
+      if (childRoute && childRoute.length > 0) {
+        realRoute!.redirect = childRoute[0].path
+        realRoute!.children = childRoute
+      }
+    }
+
+    // add item
+    if (realRoute) {
+      asyncRoutes.push(realRoute)
+    }
+  })
+  return asyncRoutes
+}
 
 /**
  * create real route raw obj
@@ -60,5 +105,14 @@ export function createRouteItem(menu: Menu, isRoot: boolean): RouteRecordRaw | n
     }
   }
 
-  return null
+  // 2级、3级以上嵌套菜单
+  return {
+    path: menu.router,
+    component: comp,
+    meta: {
+      title: menu.name,
+      icon: menu.icon,
+      noCache: !menu.keepalive,
+    },
+  }
 }
