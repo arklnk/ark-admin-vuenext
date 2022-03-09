6297957c2d51047d1ddbba51bd3ab1799787025c
hackycy <qa894178522@qq.com>
2022-03-09 18:07:58 +0800

    feat: define SidebarItem comp

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="h-full w-full relative flex flex-row">
     <RouterView />
+    <MenuItem icon="dashboard" />
+    <SvgIcon icon="a" />
   </div>
 </template>
+
+<script setup lang="ts">
+import MenuItem from './sidebar/MenuItem.vue'
+</script>
