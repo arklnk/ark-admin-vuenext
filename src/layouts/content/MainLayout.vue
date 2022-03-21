b31aa733d24217551d8971d330e45c3bc94d6454
hackycy <qa894178522@qq.com>
2022-03-21 15:47:51 +0800

    feat: main layout add transition effect

--- src/layouts/content/MainLayout.vue
+++ src/layouts/content/MainLayout.vue
@@ -1,11 +1,18 @@
 <template>
-  <main class="relative">
-    <RouterView v-if="reloadFlag" />
+  <main class="relative w-full" :class="[d.b()]">
+    <RouterView v-if="reloadFlag" v-slot="{ Component }">
+      <Transition name="fade-bottom" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </main>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useDesign } from '/@/hooks/core/useDesign'
 
 const reloadFlag = ref(true)
+
+const d = useDesign('app-main')
 </script>
