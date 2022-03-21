6121181ee5c05fd56f03f87f3ee4146b3cc08f03
hackycy <qa894178522@qq.com>
2022-03-21 17:25:29 +0800

    feat: define AppScreenfull comp

--- src/components/Application/index.ts
+++ src/components/Application/index.ts
@@ -1,6 +1,8 @@
 import AppProviderComp from './src/AppProvider.vue'
 import AppLinkComp from './src/AppLink.vue'
+import AppScreenfullComp from './src/AppScreenfull.vue'
 import { withInstall } from '/@/utils'
 
 export const AppProvider = withInstall(AppProviderComp)
 export const AppLink = withInstall(AppLinkComp)
+export const AppScreenfull = withInstall(AppScreenfullComp)
---
+++ src/components/Application/src/AppScreenfull.vue
@@ -0,0 +1,18 @@
+<template>
+  <SvgIcon
+    :class="isSupported ? 'cursor-pointer' : 'cursor-not-allowed'"
+    :icon="icon"
+    @click="handleToggleFullScreen"
+  />
+</template>
+
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core'
+import { computed, unref } from 'vue'
+
+const { isFullscreen, toggle: handleToggleFullScreen, isSupported } = useFullscreen()
+
+const icon = computed(() => {
+  return unref(isFullscreen) ? 'fullscreen-exit' : 'fullscreen'
+})
+</script>
