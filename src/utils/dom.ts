27e945938f15e61bf505c08ff7c114badd393c3b
hackycy <qa894178522@qq.com>
2022-03-23 17:28:43 +0800

    feat: define dom util

---
+++ src/utils/dom.ts
@@ -0,0 +1,14 @@
+const rootEle = document.documentElement
+
+export function setCssVar(prop: string, val: any, dom = rootEle) {
+  dom.style.setProperty(prop, val)
+}
+
+export function toggleClass(flag: boolean, clsName: string, target?: HTMLElement) {
+  const targetEl = target || document.body
+
+  let { className } = targetEl
+  className = className.replace(clsName, '')
+
+  targetEl.className = flag ? `${className} ${clsName}` : className
+}
