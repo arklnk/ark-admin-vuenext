a07f34a28c1fcdf79f06795287dcb80103195384
hackycy <qa894178522@qq.com>
2022-03-25 11:06:41 +0800

    fix: fix log error change to console

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,13 +1,13 @@
 const projectName = import.meta.env.VITE_APP_TITLE
 
 export function info(message: string) {
-  console.info(`[${projectName} info]:${message}`)
+  console.info(`[${projectName} info]: ${message}`)
 }
 
 export function warn(message: string) {
-  console.warn(`[${projectName} warn]:${message}`)
+  console.warn(`[${projectName} warn]: ${message}`)
 }
 
 export function error(message: string) {
-  throw new Error(`[${projectName} error]:${message}`)
+  console.error(`[${projectName} error]: ${message}`)
 }
