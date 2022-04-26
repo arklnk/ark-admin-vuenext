3d4cd842018538627ba64f9135e326a142e1286a
小小源逼 <qa894178522@qq.com>
2022-04-27 00:39:12 +0800

    feat: add not have permission to route, go to 403

--- src/router/guard/permissionGuard.ts
+++ src/router/guard/permissionGuard.ts
@@ -41,8 +41,16 @@ export function createPermissionGuard(router: Router) {
       await userStore.getUserInfoAction()
     }
 
-    // 如果权限路由已初始化则直接pass
+    // 如果权限路由已初始化
     if (permissionStore.getIsDynamicAddedRoute) {
+      // 检查权限路由，无任何权限则进入403
+      if (
+        permissionStore.getMenuList &&
+        permissionStore.getMenuList.length <= 0 &&
+        to.path !== PageEnum.Forbidden
+      ) {
+        return PageEnum.Forbidden
+      }
       return true
     }
 
