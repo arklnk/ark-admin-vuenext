1505e53cc9a5b32c370c6dcd719e2b767570ffde
hackycy <qa894178522@qq.com>
2022-04-26 14:42:44 +0800

    feat: impl http guard

--- src/router/guard/httpGuard.ts
+++ src/router/guard/httpGuard.ts
@@ -1,10 +1,20 @@
 import type { Router } from 'vue-router'
+
 import projectSetting from '/@/settings/projectSetting'
+import { AxiosCanceler } from '/@/utils/http/axios/axiosCanceler'
 
 export function createHttpGuard(router: Router) {
   const { removeAllHttpPending } = projectSetting
 
+  let axiosCanceler: Nullable<AxiosCanceler>
+
+  if (removeAllHttpPending) {
+    axiosCanceler = new AxiosCanceler()
+  }
+
   router.beforeEach(() => {
+    // 切换路由将删除所有的请求
+    axiosCanceler?.removeAllPending()
     return true
   })
 }
