6e6b93a6da899aa1cd769e7a292ef45c958a4a9e
hackycy <qa894178522@qq.com>
2022-04-26 17:56:22 +0800

    feat: define createMessageGuard

---
+++ src/router/guard/messageGuard.ts
@@ -0,0 +1,21 @@
+import type { Router } from 'vue-router'
+import { useMessage } from '/@/hooks/web/useMessage'
+import projectSetting from '/@/settings/projectSetting'
+import { warn } from '/@/utils/log'
+
+export function createMessageGuard(router: Router) {
+  const { closeMessageOnSwitch } = projectSetting
+  const { createMessage, createNotification } = useMessage()
+
+  router.beforeEach(() => {
+    try {
+      if (closeMessageOnSwitch) {
+        createMessage.closeAll()
+        createNotification.closeAll()
+      }
+    } catch (err) {
+      warn(`MessageGuard Error:` + err)
+    }
+    return true
+  })
+}
