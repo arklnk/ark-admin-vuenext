a127ac0908f0d1b0f43c670bbb522feb7982f2f8
hackycy <qa894178522@qq.com>
2022-04-28 16:20:24 +0800

    style: change enum item position

--- src/enums/appEnum.ts
+++ src/enums/appEnum.ts
@@ -5,10 +5,10 @@ export const MOBILE_WIDTH = 768
 
 //  Route switching animation
 export enum RouterTransitionEnum {
+  FADE = 'fade',
   ZOOM_FADE = 'zoom-fade',
   ZOOM_OUT = 'zoom-out',
   FADE_SIDE = 'fade-slide',
-  FADE = 'fade',
   FADE_BOTTOM = 'fade-bottom',
   FADE_SCALE = 'fade-scale',
 }
