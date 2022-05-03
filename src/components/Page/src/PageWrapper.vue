3b52bad2c14d974bfc73966b5f62872bd7a1b7ad
小小源逼 <qa894178522@qq.com>
2022-05-03 23:23:04 +0800

    wip: PageWrapper comp has more to do

--- src/components/Page/src/PageWrapper.vue
+++ src/components/Page/src/PageWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="prefixCls">
+  <div :class="getWrapperClass">
     <PageHeader 
       v-if="headerContent || $slots.headerContent || title" 
       :title="title">
@@ -7,14 +7,18 @@
         <template v-if="headerContent">
           <span>{{ headerContent }}</span>
         </template>
-        <slot name="headerContent"></slot>
+        <slot name="headerContent" v-else></slot>
       </template>
     </PageHeader>
+
+    <div class="overflow-hidden"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import type { CSSProperties, PropType } from 'vue'
+
+import { defineComponent, computed } from 'vue'
 import PageHeader from './PageHeader.vue'
 import { useDesign } from '/@/hooks/core/useDesign'
 
@@ -27,16 +31,41 @@ export default defineComponent({
       type: String,
       default: ''
     },
+    dense: {
+      type: Boolean,
+      default: false
+    },
     headerContent: {
       type: String,
       default: null
+    },
+    contentStyle: {
+      type: Object as PropType<CSSProperties>
+    },
+    contentClass: {
+      type: String,
+      default: ''
+    },
+    contentBackground: {
+      type: Boolean,
+      default: false
     }
   },
-  setup() {
+  setup(props, { attrs }) {
     const { prefixCls } = useDesign('page-wrapper')
 
+    const getWrapperClass = computed(() => {
+      return [
+        prefixCls,
+        {
+          [`${prefixCls}--dense`]: props.dense
+        },
+        attrs.class || {}
+      ]
+    })
+
     return {
-      prefixCls
+      getWrapperClass
     }
   }
 })
@@ -49,5 +78,19 @@ $prefixCls: #{var.$namespace}-page-wrapper;
 
 .#{$prefixCls} {
   position: relative;
+
+  &--dense {
+    .#{$prefixCls}-content {
+      margin: 0;
+    }
+  }
+
+  &-content {
+    margin: 20px;
+  }
+
+  &-content-bg {
+    background-color: var.$color-white;
+  }
 }
 </style>
