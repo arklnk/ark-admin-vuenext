8d41dee7f435c4c1cbe75d9184d98c2b1149597d
hackycy <qa894178522@qq.com>
2022-05-03 10:47:37 +0800

    feat: permission guard add catch

--- src/router/guard/permissionGuard.ts
+++ src/router/guard/permissionGuard.ts
@@ -36,42 +36,53 @@ export function createPermissionGuard(router: Router) {
       }
     }
 
-    // 判断是否需要初始化用户信息
-    if (!userStore.getUserInfo) {
-      await userStore.getUserInfoAction()
-    }
-
-    // 如果权限路由已初始化
-    if (permissionStore.getIsDynamicAddedRoute) {
-      // 检查权限路由，无任何权限则进入403
-      if (
-        permissionStore.getMenuList &&
-        permissionStore.getMenuList.length <= 0 &&
-        to.path !== PageEnum.Forbidden
-      ) {
-        return PageEnum.Forbidden
+    try {
+      // 判断是否需要初始化用户信息
+      if (!userStore.getUserInfo) {
+        await userStore.getUserInfoAction()
       }
-      return true
-    }
 
-    // 初始化权限路由
-    const routes = await permissionStore.buildRoutesAction()
-    routes.forEach((route) => {
-      router.addRoute(route)
-    })
+      // 如果权限路由已初始化
+      if (permissionStore.getIsDynamicAddedRoute) {
+        // 检查权限路由，无任何权限则进入403
+        if (
+          permissionStore.getMenuList &&
+          permissionStore.getMenuList.length <= 0 &&
+          to.path !== PageEnum.Forbidden
+        ) {
+          return PageEnum.Forbidden
+        }
+        return true
+      }
 
-    router.addRoute(NotFoundRoute)
+      // 初始化权限路由
+      const routes = await permissionStore.buildRoutesAction()
+      routes.forEach((route) => {
+        router.addRoute(route)
+      })
 
-    permissionStore.setDynamicAddedRoute(true)
+      router.addRoute(NotFoundRoute)
 
-    if (to.name === NotFoundRoute.name) {
-      // 动态添加路由后，此处应当重定向到fullPath，否则会加载404页面内容
-      return { path: to.fullPath, replace: true, query: to.query }
-    } else {
-      const redirectPath = (from.query.redirect || to.path) as string
-      const redirect = decodeURIComponent(redirectPath)
-      const nextData = to.path === redirect ? { ...to, replace: true } : { path: redirect }
-      return nextData
+      permissionStore.setDynamicAddedRoute(true)
+
+      if (to.name === NotFoundRoute.name) {
+        // 动态添加路由后，此处应当重定向到fullPath，否则会加载404页面内容
+        return { path: to.fullPath, replace: true, query: to.query }
+      } else {
+        const redirectPath = (from.query.redirect || to.path) as string
+        const redirect = decodeURIComponent(redirectPath)
+        const nextData = to.path === redirect ? { ...to, replace: true } : { path: redirect }
+        return nextData
+      }
+    } catch (e) {
+      // 菜单权限接口异常则尝试重新登录
+      return {
+        path: PageEnum.Login,
+        query: {
+          redirect: encodeURIComponent(to.fullPath),
+        },
+        replace: true,
+      }
     }
   })
 }
