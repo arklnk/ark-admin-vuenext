137b95b589a6451a755e9a4bde9ea47647050ed9
hackycy <qa894178522@qq.com>
2022-05-05 11:09:07 +0800

    feat: impl ViewNotFound view

--- src/views/error/ViewNotFound.vue
+++ src/views/error/ViewNotFound.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>页面未实现</div>
+  <PageWrapper fixed-height>
+    <ElResult class="h-full" icon="error" title="页面载入错误" sub-title="请检查对应动态载入页面路径下的文件" />
+  </PageWrapper>
 </template>
+
+<script setup lang="ts">
+import { PageWrapper } from '/@/components/Page'
+</script>
