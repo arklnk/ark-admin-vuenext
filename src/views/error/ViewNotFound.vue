aa4cc31b14c73ebae7b46d2ae997300cfc607f94
小小源逼 <qa894178522@qq.com>
2022-05-30 23:04:31 +0800

    style: remove test code

--- src/views/error/ViewNotFound.vue
+++ src/views/error/ViewNotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <PageWrapper fixed-height include-footer title="测试">
+  <PageWrapper fixed-height include-footer>
     <ElResult class="h-full" icon="error" title="页面载入错误" sub-title="请检查对应动态载入页面路径下的文件" />
   </PageWrapper>
 </template>
