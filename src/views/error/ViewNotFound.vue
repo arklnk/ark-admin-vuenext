69d41d9669d3b26ccd12c7b19f5b4d4b0e342bd9
小小源逼 <qa894178522@qq.com>
2022-06-10 13:26:14 +0800

    style: lint code

--- src/views/error/ViewNotFound.vue
+++ src/views/error/ViewNotFound.vue
@@ -1,6 +1,11 @@
 <template>
   <PageWrapper fixed-height content-full-height include-footer>
-    <ElResult class="h-full" icon="error" title="页面载入错误" sub-title="请检查对应动态载入页面路径下的文件" />
+    <ElResult
+      class="h-full"
+      icon="error"
+      title="页面载入错误"
+      sub-title="请检查对应动态载入页面路径下的文件"
+    />
   </PageWrapper>
 </template>
 
