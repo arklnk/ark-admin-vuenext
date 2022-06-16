9543aa7ceb19cf5eeba5dce2c5ac94f8483310b1
hackycy <qa894178522@qq.com>
2022-06-16 18:24:39 +0800

    wip(Table): clean old logic

--- src/components/Table/src/composables/useRowSelection.ts
+++ src/components/Table/src/composables/useRowSelection.ts
@@ -1,128 +1,50 @@
 import type { ComputedRef, Ref } from 'vue'
-import type { BasicTableProps, TableRowSelection } from '../types/table'
+import type { BasicTableProps, Key } from '../types/table'
 
-import { ref, unref, watch, computed, toRaw, nextTick } from 'vue'
-import { DEFAULT_CHILDREN_KEY, ROW_KEY } from '../const'
-import { findNodeAll } from '/@/utils/helper/tree'
-import { getRowIdentity } from '../util'
+import { unref, computed, shallowRef } from 'vue'
 
 export function useRowSelection(
   props: ComputedRef<BasicTableProps>,
-  tableData: Ref<Recordable[]>,
-  emit: EmitFn
+  _tableData: Ref<Recordable[]>,
+  _emit: EmitFn
 ) {
-  const selectedRowKeysRef = ref<string[]>([])
-  const selectedRowsRef = ref<Recordable[]>([])
-
-  watch(
-    () => unref(props).rowSelection?.selectedRowKeys,
-    (rowKeys: string[] | undefined) => {
-      if (rowKeys) {
-        setSelectedRowKeys(rowKeys as string[])
-      }
-    }
-  )
-
-  watch(
-    () => unref(selectedRowKeysRef),
-    () => {
-      nextTick(() => {
-        emit('selection-change')
-      })
-    },
-    {
-      deep: true,
-    }
-  )
-
-  const getChildrenName = computed(() => {
-    return unref(props).treeProps?.children || DEFAULT_CHILDREN_KEY
-  })
-
-  const getAutoCreateKey = computed(() => {
-    return unref(props).autoCreateKey && !unref(props).rowKey
-  })
-
-  const getRowKey = computed(() => {
-    const rowKey = unref(props).rowKey
-    return unref(getAutoCreateKey) ? ROW_KEY : (rowKey as string | ((row: any) => string))
-  })
-
-  const getRowSelectionRef = computed((): TableRowSelection | null => {
-    const { rowSelection } = unref(props)
-    if (!rowSelection) {
-      return null
-    }
+  const mergedRowSelectionRef = computed(() => {
+    const temp = unref(props).rowSelection || {}
     return {
-      selectedRowKeys: unref(selectedRowKeysRef),
-      ...rowSelection,
+      ...temp,
     }
   })
 
-  function setSelectedRowKeys(rowKeys: string[]) {
-    selectedRowKeysRef.value = rowKeys
+  const preserveRecordsCache = shallowRef<Map<Key, Recordable>>(new Map())
 
-    const allSelectedRows = findNodeAll(
-      toRaw(unref(tableData)).concat(toRaw(unref(selectedRowsRef))),
-      (row) => rowKeys.includes(getRowIdentity(row, unref(getRowKey))),
-      {
-        children: unref(getChildrenName),
-      }
-    )
+  function _updatePreserveRecordsCache(keys: Key[]) {
+    if (unref(mergedRowSelectionRef).clearOnPageChange) {
+      const newCache = new Map<Key, Recordable>()
+      // Keep key if mark as preserveSelectedRowKeys
+      keys.forEach(() => {
+        
+      })
 
-    // 去重
-    const trueSelectedRows: any[] = []
-    rowKeys.forEach((key) => {
-      const found = allSelectedRows.find((row) => getRowIdentity(row, unref(getRowKey)) === key)
-
-      found && trueSelectedRows.push(found)
-    })
-
-    selectedRowsRef.value = trueSelectedRows
-  }
-
-  function deleteSelectedRowByKey(key: string) {
-    const selectedRowKeys = unref(selectedRowKeysRef)
-    const keyIndex = selectedRowKeys.findIndex((item) => item === key)
-    if (keyIndex === -1) {
-      return
-    }
-    unref(selectedRowKeysRef).splice(keyIndex, 1)
-
-    // sync remove row
-    const selectedRows = unref(selectedRowsRef)
-    const rowIndex = selectedRows.findIndex((row) => key === getRowIdentity(row, unref(getRowKey)))
-    if (rowIndex !== -1) {
-      unref(selectedRowsRef).splice(rowIndex, 1)
+      preserveRecordsCache.value = newCache
     }
   }
 
-  function setSelectedRows(rows: Recordable[]) {
-    selectedRowsRef.value = rows
-  }
+  function _setSelectedKeys(keys: Key[]) {
+    let availableKeys: Key[]
+    let records: Recordable[]
+    if (unref(mergedRowSelectionRef).clearOnPageChange) {
+      availableKeys = keys
+      records = keys.map((key) => unref(preserveRecordsCache).get(key)!)
+    } else {
+      // Filter key which not exist in the `dataSource`
+      availableKeys = []
+      records = []
+    }
 
-  function clearSelectedRows() {
-    selectedRowKeysRef.value = []
-    selectedRowsRef.value = []
-  }
-
-  function getSelectedRowKeys(): string[] {
-    return unref(unref(selectedRowKeysRef))
-  }
-
-  function getSelectedRows<T = Recordable>() {
-    return unref(selectedRowsRef) as T[]
+    console.log(availableKeys, records)
   }
 
   return {
-    getRowSelectionRef,
-
-    getRowKey,
-    clearSelectedRows,
-    deleteSelectedRowByKey,
-    getSelectedRows,
-    setSelectedRows,
-    getSelectedRowKeys,
-    getRowIdentity,
+    mergedRowSelectionRef,
   }
 }
