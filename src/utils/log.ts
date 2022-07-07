6b20528785091801be7ab72ae08f239c242dde2f
hackycy <qa894178522@qq.com>
2022-07-07 14:51:29 +0800

    feat: warn and info log ignore in prod mode

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,10 +1,14 @@
+import { isProdMode } from './env'
+
 const projectName = import.meta.env.VITE_APP_TITLE
 
 export function info(message: string) {
+  if (isProdMode()) return
   console.info(`[${projectName} info]: ${message}`)
 }
 
 export function warn(message: string) {
+  if (isProdMode()) return
   console.warn(`[${projectName} warn]: ${message}`)
 }
 
