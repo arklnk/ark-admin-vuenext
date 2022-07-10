2194be3b195f42d261009ad0d99c4b49898d50fa
小小源逼 <qa894178522@qq.com>
2022-07-11 00:11:40 +0800

    feat: sync element-plus el-row breakpoint
    https://element-plus.gitee.io/zh-CN/component/layout.html

--- src/enums/breakpointEnum.ts
+++ src/enums/breakpointEnum.ts
@@ -4,16 +4,14 @@ export enum SizeEnum {
   MD = 'MD',
   LG = 'LG',
   XL = 'XL',
-  XXL = 'XXL',
 }
 
 export enum ScreenEnum {
   XS = 480,
-  SM = 576,
-  MD = 768,
-  LG = 992,
-  XL = 1200,
-  XXL = 1600,
+  SM = 768,
+  MD = 992,
+  LG = 1200,
+  XL = 1920,
 }
 
 const screenMap = new Map<SizeEnum, number>()
@@ -23,6 +21,5 @@ screenMap.set(SizeEnum.SM, ScreenEnum.SM)
 screenMap.set(SizeEnum.MD, ScreenEnum.MD)
 screenMap.set(SizeEnum.LG, ScreenEnum.LG)
 screenMap.set(SizeEnum.XL, ScreenEnum.XL)
-screenMap.set(SizeEnum.XXL, ScreenEnum.XXL)
 
 export { screenMap }
