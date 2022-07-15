64a6e0dcdf1adcdcbfbf906f9da657b86eb775bb
小小源逼 <qa894178522@qq.com>
2022-07-15 23:40:01 +0800

    feat: update uno theme colors config

--- uno.config.ts
+++ uno.config.ts
@@ -8,4 +8,15 @@ export default defineConfig({
     }),
   ],
   shortcuts: {},
+  theme: {
+    colors: {
+      primary: 'var(--el-color-primary)',
+      success: 'var(--el-color-success)',
+      warning: 'var(--el-color-warning)',
+      danger: 'var(--el-color-danger)',
+      error: 'var(--el-color-error)',
+      overlay: 'var(--el-bg-color-overlay)',
+      page: 'var(--el-bg-color-page)',
+    },
+  },
 })
