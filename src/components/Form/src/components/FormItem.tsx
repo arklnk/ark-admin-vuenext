f252e7caa00eb2771fe6a51c511e5cdd22c3a494
hackycy <qa894178522@qq.com>
2022-07-27 16:39:06 +0800

    feat: update changeEvent config default value

--- src/components/Form/src/components/FormItem.tsx
+++ src/components/Form/src/components/FormItem.tsx
@@ -82,7 +82,7 @@ export default defineComponent({
         render,
         size,
         modelField = 'modelValue',
-        changeEvent = 'change',
+        changeEvent = 'update:modelValue',
         prop,
       } = props.schema
 
