3270ea2679cfa4631e515455e081cbefea0957e3
小小源逼 <qa894178522@qq.com>
2022-08-07 15:12:55 +0800

    feat(Table): support custom render cell content

--- src/components/Table/src/components/TableColumn.tsx
+++ src/components/Table/src/components/TableColumn.tsx
@@ -1,7 +1,7 @@
-import type { BasicColumn } from '../types/column'
+import type { BasicColumn, TableColumnData } from '../types/column'
 
 import { defineComponent } from 'vue'
-import { omit } from 'lodash-es'
+import { isFunction, omit } from 'lodash-es'
 
 export default defineComponent({
   name: 'BasicTableColumn',
@@ -14,14 +14,17 @@ export default defineComponent({
   setup(props) {
     function renderColumns(columns: BasicColumn[]) {
       return columns.map((col) => {
-        // multiple table header
         const slots: Recordable = {}
         if (col.children && col.children.length > 0) {
+          // multiple table header
           const childColumns = renderColumns(col.children)
           slots.default = () => childColumns
+        } else if (col.render && isFunction(col.render)) {
+          // render function渲染默认内容（自定义内容需求）
+          slots.default = (scope: TableColumnData) => col.render!(scope)
         }
 
-        const bindValue = omit(col, 'children')
+        const bindValue = omit(col, ['children', 'render'])
         return <el-table-column v-slots={slots} {...bindValue} />
       })
     }
--- src/components/Table/src/types/column.ts
+++ src/components/Table/src/types/column.ts
@@ -1,10 +1,19 @@
 import type columnProps from 'element-plus/es/components/table/src/table-column/defaults'
-import type { ExtractPropTypes } from 'vue'
+import type { TableColumnCtx } from 'element-plus/es/components/table/src/table-column/defaults'
+import type { ExtractPropTypes, VNode } from 'vue'
+
+export interface TableColumnData<T = any> {
+  row: T
+  column: TableColumnCtx<T>
+  $index: number
+}
 
 /**
  * table-column-props
  */
 export interface BasicColumn extends Partial<ExtractPropTypes<typeof columnProps>> {
+  // 自定义列的内容
+  render?: (data: TableColumnData) => VNode
   // 多级表头
   children?: BasicColumn[]
 }
