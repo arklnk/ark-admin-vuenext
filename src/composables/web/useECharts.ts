fc5c05502817efe104ed66ab4411d689dab97c67
Changyuan Yang <qa894178522@qq.com>
2022-08-08 14:47:38 +0800

    feat: improve useECharts composable

--- src/composables/web/useECharts.ts
+++ src/composables/web/useECharts.ts
@@ -1,17 +1,99 @@
 import type { Ref } from 'vue'
 import type { ThemeEnum } from '/@/enums/appEnum'
+import type { EChartsOption } from 'echarts'
 
-import { computed } from 'vue'
+import { computed, unref, watch, nextTick } from 'vue'
+import { tryOnUnmounted } from '@vueuse/core'
 import { useRootSetting } from '/@/composables/setting/useRootSetting'
+import echarts from '/@/utils/lib/echarts'
+import { useWindowSizeFn } from '/@/composables/event/useWindowSizeFn'
 
-export function useECharts(_elRef: Ref<Nullable<HTMLDivElement>>, theme: ThemeEnum | 'default') {
+export function useECharts(
+  elRef: Ref<Nullable<HTMLDivElement>>,
+  theme: ThemeEnum | 'default' = 'default'
+) {
   const { getTheme } = useRootSetting()
-
   const getDarkMode = computed(() => {
     return theme === 'default' ? getTheme.value : theme
   })
 
+  let chartsInstance: echarts.ECharts | null = null
+  // 记录最近一次setOption的配置
+  let cacheOption: EChartsOption = {}
+
+  function initCharts() {
+    const el = unref(elRef)
+    if (!el) return
+
+    // 初始化
+    chartsInstance = echarts.init(el, unref(getDarkMode))
+
+    // window resize event
+    useWindowSizeFn(resize, 200)
+  }
+
+  function getOption(): EChartsOption {
+    // 合并默认配置，取消dark下的backgroundColor
+    return {
+      backgroundColor: 'transparent',
+      ...(cacheOption || {}),
+    }
+  }
+
+  function setOption(option: EChartsOption, clear = true) {
+    cacheOption = option
+
+    nextTick(() => {
+      if (!chartsInstance) {
+        initCharts()
+
+        if (!chartsInstance) return
+      }
+
+      clear && chartsInstance?.clear()
+
+      chartsInstance?.setOption(getOption())
+    })
+  }
+
+  function resize() {
+    chartsInstance?.resize({
+      animation: {
+        duration: 500,
+        easing: 'quadraticIn',
+      },
+    })
+  }
+
+  function getChartsInstance(): echarts.ECharts | null {
+    if (!chartsInstance) {
+      initCharts()
+    }
+    return chartsInstance
+  }
+
+  watch(
+    () => unref(getDarkMode),
+    () => {
+      if (!chartsInstance) return
+
+      chartsInstance.dispose()
+      initCharts()
+      setOption(getOption())
+    }
+  )
+
+  tryOnUnmounted(() => {
+    if (!chartsInstance) return
+
+    chartsInstance.dispose()
+    chartsInstance = null
+  })
+
   return {
-    getDarkMode,
+    initCharts,
+    resize,
+    setOption,
+    getChartsInstance,
   }
 }
--- src/utils/lib/echarts.ts
+++ src/utils/lib/echarts.ts
@@ -7,8 +7,67 @@ import * as echarts from 'echarts/core'
  */
 import { SVGRenderer } from 'echarts/renderers'
 
+// 组件
+import {
+  TitleComponent,
+  TooltipComponent,
+  ToolboxComponent,
+  LegendComponent,
+  PolarComponent,
+  DatasetComponent,
+  VisualMapComponent,
+  TransformComponent,
+  GridComponent,
+  CalendarComponent,
+  DataZoomComponent,
+  TimelineComponent,
+  AriaComponent,
+  AxisPointerComponent,
+} from 'echarts/components'
+
+// 图表
+import {
+  BarChart,
+  LineChart,
+  PieChart,
+  ScatterChart,
+  RadarChart,
+  GaugeChart,
+  MapChart,
+  PictorialBarChart,
+} from 'echarts/charts'
+
 // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
 // 注册必须的组件
-echarts.use([SVGRenderer])
+echarts.use([
+  // render
+  SVGRenderer,
+
+  // component
+  TitleComponent,
+  TooltipComponent,
+  ToolboxComponent,
+  LegendComponent,
+  PolarComponent,
+  DatasetComponent,
+  VisualMapComponent,
+  TransformComponent,
+  GridComponent,
+  CalendarComponent,
+  DataZoomComponent,
+  TimelineComponent,
+  AriaComponent,
+  AxisPointerComponent,
+
+  // charts
+  BarChart,
+  LineChart,
+  PieChart,
+  ScatterChart,
+  RadarChart,
+  GaugeChart,
+  MapChart,
+  PictorialBarChart,
+])
 
 export default echarts
