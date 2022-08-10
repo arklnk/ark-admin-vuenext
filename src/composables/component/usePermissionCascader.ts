96624f3365ee330edc7245bb7109c1965e7e1d29
Changyuan Yang <qa894178522@qq.com>
2022-08-10 17:01:16 +0800

    feat: improve permissonHelper and define usePermissionCascader composable

---
+++ src/composables/component/usePermissionCascader.ts
@@ -0,0 +1,76 @@
+import { computed } from 'vue'
+import { getDefinePermissionList } from '/@/logics/permission/permissonHelper'
+
+export interface Config {
+  value?: string
+  label?: string
+  children?: string
+}
+
+const SEPARATOR = '/'
+
+export function usePermissionCascader(config?: Config) {
+  // 所有定义的权限集合：['sys/user/info', 'sys/user/add']
+  const perms = getDefinePermissionList()
+
+  const getConfig = computed((): Config => {
+    return {
+      value: 'value',
+      label: 'label',
+      children: 'children',
+      ...(config || {}),
+    }
+  })
+
+  function dig(start: number, cur: string[], op: Recordable[]) {
+    const label = cur[start]
+    // 查找options是否已经被重复定义
+    const index = op.findIndex((e) => e.label === label)
+
+    // 已经被定义，那么往下查询子节点
+    if (index >= 0) {
+      dig(start + 1, cur, op[index].children!)
+      return
+    }
+
+    // 判断是否为最后一个字节点，防止超出边界
+    const isLast = start === cur.length - 1
+
+    const option: Recordable = {
+      [getConfig.value.label!]: label,
+      [getConfig.value.value!]: label,
+      [getConfig.value.children!]: isLast ? undefined : [],
+    }
+
+    if (op) {
+      op.push(option)
+    }
+
+    if (!isLast) {
+      dig(start + 1, cur, op[op.length - 1].children!)
+    }
+  }
+
+  const getOptionsRef = computed((): Recordable[] => {
+    const auth: Recordable[] = []
+
+    perms.forEach((item) => {
+      const s = item.startsWith(SEPARATOR) ? item.replace(SEPARATOR, '') : item
+
+      const arr = s.split(SEPARATOR)
+
+      dig(0, arr, auth)
+    })
+
+    return auth
+  })
+
+  function transformValues(values: (string | number)[][], separator = '/'): string[] {
+    return values.map((e) => e.join(`${separator}`))
+  }
+
+  return {
+    getOptionsRef,
+    transformValues,
+  }
+}
---
+++ src/logics/permission/permissonHelper.ts
@@ -0,0 +1,38 @@
+import { isEmpty, isPlainObject, uniq } from 'lodash-es'
+
+/**
+ * 获取所有已经定义的权限api, 为区分普通无权限的api文件定义，需要权限的均已.api.ts文件后缀命名
+ */
+type ApiRecord = {
+  Api?: Recordable<string>
+}
+let definePermissionList: string[] | null = null
+
+export function getDefinePermissionList(): string[] {
+  if (definePermissionList === null) {
+    // define permssion api modules
+    const defineModules: Record<string, ApiRecord> = import.meta.globEager('../../api/**/*.api.ts')
+
+    const keys = Object.keys(defineModules)
+    let permList: string[] = []
+
+    keys.forEach((k) => {
+      const define = defineModules[k]
+
+      if (!isPlainObject(define.Api) || isEmpty(define.Api)) return
+
+      const api = Object.values(define.Api!)
+      if (Array.isArray(api) && api.length > 0) {
+        permList.push(...api)
+      }
+    })
+
+    // 过滤重复定义的api
+    permList = uniq(permList)
+
+    // 缓存
+    definePermissionList = permList
+  }
+
+  return definePermissionList
+}
