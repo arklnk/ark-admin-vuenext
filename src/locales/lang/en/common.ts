6a455d28639afe11c12d2d2481d4c94f9234a88a
小小源逼 <qa894178522@qq.com>
2022-08-15 22:04:24 +0800

    feat(i18n): http util i18n translate

--- src/locales/lang/en/common.ts
+++ src/locales/lang/en/common.ts
@@ -1,3 +1,17 @@
 export default {
   appName: 'Ark Admin',
+
+  // http util
+  http: {
+    errorTip: 'Error tip',
+    requestFailed: 'The interface request failed, please try again later!',
+    requestTimeout: 'The interface request timed out, please refresh the page and try again!',
+    networkException: 'Please check if your network connection is normal! The network is abnormal',
+
+    errMsg403: 'The user is authorized, but access is forbidden!',
+    errMsg404: 'Network request error, the resource was not found!',
+    errMsg405: 'Network request error, request method not allowed!',
+    errMsg500: 'Server error, please contact the administrator!',
+    errMsg503: 'The service is unavailable, the server is temporarily overloaded or maintained!',
+  },
 }
--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -12,9 +12,7 @@ import { getToken } from '../../auth'
 import { useUserStore } from '/@/stores/modules/user'
 import { usePermissionStore } from '/@/stores/modules/permission'
 import { AxiosRetry } from './axiosRetry'
-
-const UnknownErrorMsg = '未知错误,请稍候重试!'
-const ErrorTip = '错误提示'
+import { useTransl } from '/@/composables/core/useTransl'
 
 const { createMessage, createMessageBox } = useMessage()
 
@@ -23,6 +21,7 @@ const transform: AxiosTransform = {
    * @description 处理请求数据，将接口数据转换成所需的格式
    */
   transformResponseHook: (res: AxiosResponse<Result>, options: RequestOptions): any => {
+    const { t } = useTransl()
     const { isTransformResponse, isReturnNativeResponse, errorMessageMode } = options
     // 返回原生响应
     if (isReturnNativeResponse) {
@@ -35,7 +34,7 @@ const transform: AxiosTransform = {
 
     const { data } = res
     if (!data) {
-      throw new Error(UnknownErrorMsg)
+      throw new Error('response data empty')
     }
 
     const { data: result, msg: message, code } = data
@@ -47,7 +46,7 @@ const transform: AxiosTransform = {
     }
 
     // 错误描述信息
-    let errMessage = ''
+    const errMessage = `[${code}] ${message || t('common.http.requestFailed')}`
 
     // 在此处根据自己项目的实际情况对不同的code执行不同的操作
     // 如果不希望中断当前请求，请return数据，否则直接抛出异常即可
@@ -60,11 +59,7 @@ const transform: AxiosTransform = {
 
         userStore.resetState()
         permissionStore.resetState()
-
-        errMessage = `[${code}] ${message},请重新登录`
         break
-      default:
-        errMessage = `[${code}] ${message}`
     }
 
     /**
@@ -72,13 +67,15 @@ const transform: AxiosTransform = {
      * errorMessageMode=‘message’的时候会显示Message错误弹窗，而不是错误弹窗，用于一些比较简单的错误
      */
     if (errorMessageMode === 'messageBox') {
-      createMessageBox.alert(errMessage, ErrorTip, { type: 'error' }).catch(() => {})
+      createMessageBox
+        .alert(errMessage, t('common.http.errorTip'), { type: 'error' })
+        .catch(() => {})
     } else if (errorMessageMode === 'message') {
       createMessage.error(errMessage)
     }
 
     // throw
-    throw new Error(errMessage || UnknownErrorMsg)
+    throw new Error(errMessage)
   },
 
   /**
@@ -140,6 +137,7 @@ const transform: AxiosTransform = {
    * 响应错误处理拦截器
    */
   responseInterceptorsCatch: (axiosInstance: AxiosInstance, error: any) => {
+    const { t } = useTransl()
     const { config, response, message, code } = (error as AxiosError) || {}
 
     const errorMessageMode =
@@ -151,32 +149,33 @@ const transform: AxiosTransform = {
 
     // axios timeout
     if (code === 'ECONNABORTED' && message?.indexOf('timeout') !== -1) {
-      errMessage = '接口请求超时,请刷新页面重试!'
+      errMessage = t('common.http.requestTimeout')
     }
 
     if (err.includes('Network Error')) {
-      errMessage = '网络异常，请检查您的网络连接是否正常!'
+      errMessage = t('common.http.networkException')
     }
 
     // 检查status
     const status = response?.status
     switch (status) {
-      case 404:
-        errMessage = '网络请求错误,未找到该资源!'
+      case 403:
+        errMessage = t('common.http.errMsg403')
         break
-      case 422:
-        errMessage = '参数错误,服务器无法响应!'
+      case 404:
+        errMessage = t('common.http.errMsg404')
         break
       case 405:
-        errMessage = '网络请求错误,请求方法未允许!'
+        errMessage = t('common.http.errMsg405')
         break
       case 500:
-        errMessage = '服务器错误,请联系管理员!'
+        errMessage = t('common.http.errMsg500')
         break
       case 503:
-        errMessage = '服务不可用，服务器暂时过载或维护!'
+        errMessage = t('common.http.errMsg503')
         break
       default:
+        errMessage = t('common.http.requestFailed')
     }
 
     /**
@@ -184,7 +183,9 @@ const transform: AxiosTransform = {
      * errorMessageMode=‘message’的时候会显示Message错误弹窗，而不是错误弹窗，用于一些比较简单的错误
      */
     if (errorMessageMode === 'messageBox') {
-      createMessageBox.alert(errMessage, ErrorTip, { type: 'error' }).catch(() => {})
+      createMessageBox
+        .alert(errMessage, t('common.http.errorTip'), { type: 'error' })
+        .catch(() => {})
     } else if (errorMessageMode === 'message') {
       createMessage.error(errMessage)
     }
