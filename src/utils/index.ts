36aff488485b704eff19db54006d03c868be4585
Changyuan Yang <qa894178522@qq.com>
2022-08-15 14:34:37 +0800

    style: remove unless util func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,5 @@
 import type { App, Plugin } from 'vue'
 
-import { isEmpty } from 'lodash-es'
-
-const defaultTitle = import.meta.env.VITE_APP_TITLE
-
-/**
- * 获取格式化的document标题文本
- */
-export function getPageTitle(subTitle: string | undefined | null) {
-  if (isEmpty(subTitle)) {
-    return defaultTitle
-  }
-  return `${subTitle} - ${defaultTitle}`
-}
-
 /**
  * / _ - 符号转换成驼峰
  */
