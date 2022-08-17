b30188a11fac23e5113dcc6e19ff70b1105091d3
小小源逼 <qa894178522@qq.com>
2022-08-17 23:22:25 +0800

    feat: unsing tailwind reset css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import '@unocss/reset/normalize.css'
+import '@unocss/reset/tailwind.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'uno.css'
