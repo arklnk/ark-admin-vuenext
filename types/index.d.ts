35299c7af888cce06293a5d0fd25e8c45869939f
Changyuan Yang <qa894178522@qq.com>
2022-08-17 15:15:52 +0800

    feat: define typo

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,10 @@ declare interface Fn<T = any, R = T> {
   (...arg: T[]): R
 }
 
+declare interface PromiseFn<T = any, R = T> {
+  (...arg: T[]): Promise<R>
+}
+
 declare type EmitFn = (event: any, ...args: any[]) => void
 
 declare type WindowTargetContext = '_self' | '_blank'
