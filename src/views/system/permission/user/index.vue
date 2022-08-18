5f85ee38de69d8e34b1964f6086e9c4a1c3eb119
小小源逼 <qa894178522@qq.com>
2022-08-19 00:17:45 +0800

    feat: define user view

---
+++ src/views/system/permission/user/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <PageWrapper :content-background="false" content-full-height fixed-height>
+    <div class="flex flex-row h-full relative">
+      <div class="mr-2 h-full w-[300px] bg-white dark:bg-overlay">部门列表</div>
+      <div class="flex-1 h-full bg-white dark:bg-overlay">用户列表</div>
+    </div>
+  </PageWrapper>
+</template>
+
+<script setup lang="ts">
+import { PageWrapper } from '/@/components/Page'
+</script>
