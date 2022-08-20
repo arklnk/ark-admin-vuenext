033711cc8df0de7f862b7f0f45d3e43a334d41cb
小小源逼 <qa894178522@qq.com>
2022-08-21 00:34:14 +0800

    feat(Input): define I18nInput comp

---
+++ src/components/Input/index.ts
@@ -0,0 +1,4 @@
+import I18nInputComp from './src/I18nInput.vue'
+import { withInstall } from '/@/utils'
+
+export const I18nInput = withInstall(I18nInputComp)
---
+++ src/components/Input/src/I18nInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <ElInput type="text" :model-value="modelValue" @update:model-value="handleUpdate">
+    <template #append>{{ translateText }}</template>
+  </ElInput>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useTransl } from '/@/composables/core/useTransl'
+
+const props = defineProps({
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function handleUpdate(v: string) {
+  emit('update:modelValue', v)
+}
+
+const { t } = useTransl()
+
+const translateText = computed(() => t(props.modelValue || ''))
+</script>
