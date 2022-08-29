22a56290db3261033d54ef714e6d4636b4d924d2
Changyuan Yang <qa894178522@qq.com>
2022-08-29 11:50:17 +0800

    feat: update dashboard default page

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,3 +1,33 @@
 <template>
-  <div>dashboard</div>
+  <PageWrapper :content-background="false">
+    <ElCard class="px-4">
+      <div class="font-semibold my-4 text-xl">您好, {{ getUsername }}, 请开始一天的工作吧!</div>
+      <div class="text-gray-500 my-4 text-sm flex items-center">
+        使用文档: <ElLink class="ml-4" type="primary" :href="DOC_SITE">{{ DOC_SITE }}</ElLink>
+      </div>
+      <div class="text-gray-500 my-4 text-sm flex items-center">
+        前端仓库:
+        <ElLink class="ml-4" type="primary" :href="VUE_REPO_GITHUB_URL">{{
+          VUE_REPO_GITHUB_URL
+        }}</ElLink>
+      </div>
+      <div class="text-gray-500 my-4 text-sm flex items-center">
+        使用文档:
+        <ElLink class="ml-4" type="primary" :href="ZERO_REPO_GITHUB_URL">{{
+          ZERO_REPO_GITHUB_URL
+        }}</ElLink>
+      </div>
+    </ElCard>
+  </PageWrapper>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { PageWrapper } from '/@/components/Page'
+import { DOC_SITE, VUE_REPO_GITHUB_URL, ZERO_REPO_GITHUB_URL } from '/@/settings/siteSetting'
+import { useUserStore } from '/@/stores/modules/user'
+
+const userStore = useUserStore()
+
+const getUsername = computed(() => userStore.getUserInfo?.username)
+</script>
