6c7ea36a07acd35744d5c1ff38aebaecbcb572f5
Changyuan Yang <qa894178522@qq.com>
2022-08-29 12:59:08 +0800

    style: update site setting

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,6 +1,6 @@
 // github repo url
 export const VUE_REPO_GITHUB_URL = 'https://github.com/arklnk/ark-admin-vuenext'
 export const ZERO_REPO_GITHUB_URL = 'https://github.com/arklnk/ark-admin-zero'
-export const DOC_SITE = 'https://docs.arklnk.com/'
+export const DOC_SITE = 'https://docs.arklnk.com'
 // site url
 export const SITE_URL = 'http://opensource.admin.si-yee.com'
