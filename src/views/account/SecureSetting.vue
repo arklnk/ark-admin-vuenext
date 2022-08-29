7376cb032622e4ba981274efdbd3908317c038c9
小小源逼 <qa894178522@qq.com>
2022-08-29 23:27:54 +0800

    pref: simplify account view code

--- src/views/account/SecureSetting.vue
+++ src/views/account/SecureSetting.vue
@@ -1,29 +1,70 @@
 <template>
-  <BasicForm
-    class="w-[450px] mt-4 ml-6"
-    :show-reset-button="false"
-    :show-submit-button="false"
-    :schemas="profileSchemas"
-    label-position="top"
-    @register="registerForm"
-    @submit="handleSubmit"
-  >
+  <BasicForm class="w-[450px] mt-4 ml-6" @register="registerForm" @submit="handleSubmit">
     <template #submitBefore>
-      <ElButton type="success" @click="submit">更新密码</ElButton>
+      <ElButton type="primary" @click="submit">更新密码</ElButton>
     </template>
   </BasicForm>
 </template>
 
 <script setup lang="ts">
-import type { FormSchema } from '/@/components/Form'
-
 import { BasicForm, useForm } from '/@/components/Form'
-
-import { ref } from 'vue'
 import { updateUserPasswd } from '/@/api/user'
 import { useMessage } from '/@/composables/web/useMessage'
 
-const [registerForm, { submit, setProps, getFieldsValue, resetFields }] = useForm()
+const [registerForm, { submit, setProps, getFieldsValue, resetFields }] = useForm({
+  showResetButton: false,
+  showSubmitButton: false,
+  labelPosition: 'top',
+  schemas: [
+    {
+      prop: 'oldPassword',
+      label: '旧密码',
+      defaultValue: '',
+      component: 'ElInput',
+      rules: {
+        required: true,
+        type: 'string',
+        min: 6,
+        max: 12,
+        message: '请输入旧密码,长度6-12位',
+        trigger: 'blur',
+      },
+    },
+    {
+      prop: 'newPassword',
+      label: '新密码',
+      defaultValue: '',
+      component: 'ElInput',
+      rules: {
+        required: true,
+        type: 'string',
+        min: 6,
+        max: 12,
+        message: '请输入新密码,长度6-12位',
+        trigger: 'blur',
+      },
+    },
+    {
+      prop: 'confirmNewPassword',
+      label: '确认密码',
+      defaultValue: '',
+      component: 'ElInput',
+      rules: {
+        required: true,
+        trigger: 'blur',
+        validator: (_, value: string, cb: Fn) => {
+          if (!value) {
+            cb(new Error('请再次输入密码以确认'))
+          } else if (value !== getFieldsValue().newPassword) {
+            cb(new Error('二次确认密码不一致'))
+          } else {
+            cb()
+          }
+        },
+      },
+    },
+  ],
+})
 
 const { createMessage } = useMessage()
 
@@ -46,52 +87,4 @@ async function handleSubmit(res: any) {
     setProps({ disabled: false })
   }
 }
-
-const profileSchemas = ref<FormSchema[]>([
-  {
-    prop: 'oldPassword',
-    label: '旧密码',
-    defaultValue: '',
-    component: 'ElInput',
-    rules: {
-      required: true,
-      type: 'string',
-      min: 6,
-      max: 12,
-      message: '请输入旧密码,长度6-12位',
-      trigger: 'blur',
-    },
-  },
-  {
-    prop: 'newPassword',
-    label: '新密码',
-    defaultValue: '',
-    component: 'ElInput',
-    rules: {
-      required: true,
-      type: 'string',
-      min: 6,
-      max: 12,
-      message: '请输入新密码,长度6-12位',
-      trigger: 'blur',
-    },
-  },
-  {
-    prop: 'confirmNewPassword',
-    label: '确认密码',
-    defaultValue: '',
-    component: 'ElInput',
-    rules: {
-      required: true,
-      trigger: 'blur',
-      validator: (_, value: string, cb: Fn) => {
-        if (value !== getFieldsValue().newPassword) {
-          cb(new Error('二次确认密码不一致'))
-        } else {
-          cb()
-        }
-      },
-    },
-  },
-])
 </script>
