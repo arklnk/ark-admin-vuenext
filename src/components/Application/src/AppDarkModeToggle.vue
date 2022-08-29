afc173009b7c5d0dcf84590c3bf2fd5e51e6849e
Changyuan Yang <qa894178522@qq.com>
2022-08-29 13:01:18 +0800

    feat(App): beautification AppDarkModeToggle comp

--- src/components/Application/src/AppDarkModeToggle.vue
+++ src/components/Application/src/AppDarkModeToggle.vue
@@ -1,21 +1,32 @@
 <template>
-  <span
-    class="text-center cursor-pointer text-yellow-300 dark:text-purple-300"
-    @click="toggleDarkMode"
-  >
-    <span class="i-ic:baseline-wb-sunny" v-if="getTheme === ThemeEnum.LIGHT"></span>
-    <span class="i-ic:outline-dark-mode" v-else></span>
+  <span class="text-center">
+    <ElSwitch
+      @change="toggleDarkMode()"
+      :active-value="ThemeEnum.DARK"
+      :inactive-value="ThemeEnum.LIGHT"
+      :active-icon="darkIcon"
+      :inactive-icon="lightIcon"
+      inline-prompt
+      :model-value="getTheme"
+      :style="{
+        '--el-switch-on-color': '#141414',
+        '--el-switch-off-color': '#141414',
+      }"
+    />
   </span>
 </template>
 
 <script setup lang="ts">
-import { unref } from 'vue'
+import { h, unref } from 'vue'
 import { updateHeaderBgColor, updateSidebarBgColor } from '/@/logics/theme/updateBackground'
 import { updateDarkMode } from '/@/logics/theme/updateDarkMode'
 import { ThemeEnum } from '/@/enums/appEnum'
 import { useRootSetting } from '/@/composables/setting/useRootSetting'
 
 const { getTheme, setDarkMode } = useRootSetting()
+
+const lightIcon = h('span', { class: 'i-ic:baseline-wb-sunny text-yellow-300' })
+const darkIcon = h('span', { class: 'i-ic:outline-dark-mode text-purple-300' })
 
 function toggleDarkMode() {
   const darkMode = unref(getTheme) === ThemeEnum.LIGHT ? ThemeEnum.DARK : ThemeEnum.LIGHT
