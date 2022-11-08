37f26ff52d8aa3878fc6a57abc40119f2e041290
小小源逼 <qa894178522@qq.com>
2022-11-08 23:48:22 +0800

    style: add nestjs repo link

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,6 +1,7 @@
 // github repo url
 export const VUE_REPO_GITHUB_URL = 'https://github.com/arklnk/ark-admin-vuenext'
 export const ZERO_REPO_GITHUB_URL = 'https://github.com/arklnk/ark-admin-zero'
+export const NEST_REPO_GITHUB_URL = 'https://github.com/arklnk/ark-admin-nest'
 export const DOC_SITE = 'https://docs.arklnk.com'
 // site url
 export const SITE_URL = 'http://opensource.admin.si-yee.com'
