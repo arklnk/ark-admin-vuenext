d0102f5a1f052b6539964efbaf22dcecfc05adb7
小小源逼 <qa894178522@qq.com>
2022-11-09 19:53:02 +0800

    revert: revert (7662ee4)

--- src/views/system/menu/schemas.tsx
+++ src/views/system/menu/schemas.tsx
@@ -90,10 +90,6 @@ export function createSchemas(): FormSchema[] {
       label: '视图路径',
       defaultValue: '',
       prop: 'viewPath',
-      rules: {
-        required: true,
-        type: 'string',
-      },
       hidden: ({ model }) => {
         return model.type === 2 || model.type === 0
       },
